6c8dd9c7c18e6d0ecefd130097d2ac5a9ffdeeb8
Francisco M <120040483+fmellado@users.noreply.github.com>
2023-12-08 16:45:59 -0500

    Add files via upload

---
+++ Template/Q1Lab4/Program.cs
@@ -0,0 +1,151 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Q1Lab4;
+using System;
+using System.Data.SqlClient;
+
+Console.WriteLine("Francisco Mellado, 301304340 /n /n");
+Console.WriteLine("Question 01 - Lab 04 /n" );
+;
+
+var countries = Country.GetCountries();
+
+// Invokes methods
+Question1_1();
+Question1_2();
+Question1_3();
+Question1_4();
+Question1_5();
+Question1_6();
+
+
+// 1.1 List the names of the countries in alphabetical order [0.5 mark]
+void Question1_1()
+{
+    try
+    {
+        var alphabetCountries = from country in countries
+                                orderby country.Name
+                                select country.Name;
+        Console.WriteLine("Question 1_1: ");
+        foreach (var Nombre in alphabetCountries)
+        {
+            Console.WriteLine(Nombre);
+        }
+        Console.WriteLine($"\n");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+    }
+}
+
+    // 1.2 List the names of the countries in descending order of number of resources [0.5 mark]
+    void Question1_2()
+    {
+
+        try
+        {
+
+
+            var countriesByResources = from country in countries
+                                       orderby country.Resources.Count descending
+                                       select country.Name;
+            Console.WriteLine($"Question 1_2:");
+            foreach (var countryName in countriesByResources)
+            {
+                Console.WriteLine(countryName);
+            }
+            Console.WriteLine($"\n");
+        }
+        catch (Exception ex) { Console.WriteLine($"An error occurred: {ex.Message}"); }
+    }
+
+    // 1.3 List the names of the countries that shares a border with Argentina [0.5 mark]
+    void Question1_3()
+    {
+
+    try
+        {
+            var argentinaNeighbors = from country in countries
+                                     where country.Borders.Contains("Argentina")
+                                     select country.Name;
+            Console.WriteLine($"Question 1_3:");
+            foreach (var countryName in argentinaNeighbors)
+            {
+                Console.WriteLine(countryName);
+            }
+            Console.WriteLine($"\n");
+        }
+        catch (Exception ex) { Console.WriteLine($"An error occurred: {ex.Message}"); }
+    }
+
+
+    // 1.4 List the names of the countries that has more than 10,000,000 population [0.5 mark]
+    void Question1_4()
+    {
+
+    try
+        {
+            var highPopulationCountries = from country in countries
+                                          where country.Population > 10000000
+                                          select country.Name;
+            Console.WriteLine($"Question 1_4:");
+            foreach (var countryName in highPopulationCountries)
+            {
+                Console.WriteLine(countryName);
+            }
+            Console.WriteLine($"\n");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+
+    // 1.5 List the country with highest population [1 mark]
+    void Question1_5()
+    {
+
+    if (countries != null && countries.Any())
+        {
+            var highestPopulation = (from country in countries
+                                     orderby country.Population descending
+                                     select country.Name).FirstOrDefault();
+
+            Console.WriteLine($"Question 1_5:");
+            Console.WriteLine(highestPopulation);
+            Console.WriteLine("\n");
+        }
+        else
+        {
+            Console.WriteLine("No countries available.");
+        }
+    }
+
+    // 1.6 List all the religion in south America in dictionary order [1 mark]
+    void Question1_6()
+    {
+
+    try
+        {
+            var countries = Country.GetCountries();
+
+            // Selecciona todas las religiones de cada país, luego aplanamos la lista y la ordenamos
+            var religions = (from country in countries
+                             from religion in country.Religions
+                             orderby religion
+                             select religion).Distinct().ToList();
+
+            Console.WriteLine("Religiones en Sudamérica en orden de diccionario:");
+            foreach (var religion in religions)
+            {
+                Console.WriteLine(religion);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Se produjo un error: {ex.Message}");
+        }
+    }
+
---
+++ Template/Q2Lab4/Program.cs
@@ -0,0 +1,141 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data.SqlClient;
+
+
+Console.WriteLine("Question 02 - Lab 04");
+string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\fmell\\OneDrive\\Centenial Progress\\FALL 23\\Programing 3\\Lab4\\Template\\Q2Lab4\\Database\\Books.mdf\";Integrated Security=True";
+
+// Invokes methods
+Question2_1();
+Question2_2();
+Question2_3();
+
+//1.Get a list of all the titles and the authors who wrote them. Sort the results by title. [2 marks]
+void Question2_1()
+{
+    string query = @"SELECT t.Title, a.FirstName, a.LastName
+                     FROM Titles t
+                     JOIN AuthorISBN ai ON t.ISBN = ai.ISBN
+                     JOIN Authors a ON ai.AuthorID = a.AuthorID
+                     ORDER BY t.Title ASC;";
+
+    Console.WriteLine($"Question 2_1:");
+    Console.WriteLine("List of all the titles and the authors who wrote them. Sort the results by title");
+    Console.WriteLine();
+    try
+    { 
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        SqlCommand command = new SqlCommand(query, connection);
+        connection.Open();
+
+        using (SqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                string title = reader["Title"].ToString();
+                string firstName = reader["FirstName"].ToString();
+                string lastName = reader["LastName"].ToString();
+
+                Console.WriteLine($"TIttle: {title} || by {firstName} {lastName}");
+            }
+        }
+    }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred when executing the query: {ex.Message}");
+    }
+    Console.WriteLine($"\n");
+    Console.ReadLine();
+}
+
+//2.Get a list of all the titles and the authors who wrote them. Sort the results by title.  Each title sort the authors alphabetically by last name, then first name[4 marks]
+void Question2_2()
+{
+    string query = @"SELECT t.Title, a.FirstName, a.LastName
+                     FROM Titles t
+                     JOIN AuthorISBN ai ON t.ISBN = ai.ISBN
+                     JOIN Authors a ON ai.AuthorID = a.AuthorID
+                     ORDER BY t.Title ASC, a.LastName ASC, a.FirstName ASC;";
+
+    
+    Console.WriteLine($"Question 2.2:");
+    Console.WriteLine("List of all the titles and the authors who wrote them. Sort the results by title.  Each title sort the authors alphabetically by last name, then first name");
+    Console.WriteLine();
+    try
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string title = reader["Title"].ToString();
+                    string firstName = reader["FirstName"].ToString();
+                    string lastName = reader["LastName"].ToString();
+
+                    Console.WriteLine($"Title: {title} || by {lastName}, {firstName}");
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Se produjo un error al ejecutar la consulta: {ex.Message}");
+    }
+    Console.WriteLine($"\n");
+    Console.ReadLine();
+}
+
+//3.Get a list of all the authors grouped by title, sorted by title; for a given title sort the author names alphabetically by last name then first name.[4 marks]
+    void Question2_3()
+    {
+        string query = @"SELECT t.Title, a.FirstName, a.LastName
+                     FROM Titles t
+                     JOIN AuthorISBN ai ON t.ISBN = ai.ISBN
+                     JOIN Authors a ON ai.AuthorID = a.AuthorID
+                     GROUP BY t.Title, a.FirstName, a.LastName
+                     ORDER BY t.Title ASC, a.LastName ASC, a.FirstName ASC;";
+
+    Console.WriteLine("List of all the authors grouped by title, sorted by title");
+    Console.WriteLine($"Question 2_3:");
+        Console.WriteLine();
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    string currentTitle = "";
+                    while (reader.Read())
+                    {
+                        string title = reader["Title"].ToString();
+                        string firstName = reader["FirstName"].ToString();
+                        string lastName = reader["LastName"].ToString();
+
+                        if (title != currentTitle)
+                        {
+                            Console.WriteLine($"\n{title}:");
+                            currentTitle = title;
+                        }
+
+                        Console.WriteLine($"\t{lastName}, {firstName}");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Se produjo un error al ejecutar la consulta: {ex.Message}");
+        }
+        Console.WriteLine($"\n");
+        Console.ReadLine();
+    }
